76bbbe60dad796693486333fb678faec65be60cf
Hildi <s164539@student.dtu.dk>
2021-09-15 15:51:56 +0200

    text added

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main() {
     int var = 0;
     printf("I know print %d this var", var);
     printf("This a little less of var %d",var-5);
+    printf("Please don't kille your children\n");
     return 0;
 }
 /**
