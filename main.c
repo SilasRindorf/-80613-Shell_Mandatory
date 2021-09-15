e628b25d9dc2ce401900a2e5b8f60ac61ae77da4
SilasRindorf <s195474@student.dtu.dk>
2021-09-15 15:49:51 +0200

    Another commit

--- main.c
+++ main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+/***
+ *
+ * @return
+ */
 int main() {
-    printf("Hello, World!\n");
-    int var = 0;
-    printf("I know print %d this var", var);
+    printf("Parent process booting up with pid=%d!\n",getpid());
+    int rc = fork();
+    if (rc < 0){
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0){}
+
+    printf("I know print %d this var\n", getpid());
     return 0;
 }
+
+
 /**
+ * {}
  * Sejr GitTest
  */
