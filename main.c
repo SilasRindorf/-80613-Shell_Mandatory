481bb1c3fe9a6ea8c0c8bfa107b4021fed3d28ac
MortencKruuse <Hiccup#1998>
2021-09-22 16:06:04 +0200

    Commit for SIlas

--- main.c
+++ main.c
@@ -5,6 +5,38 @@
 #include <string.h>
 #include <limits.h>
 
+
+
+/*
+ * int main() {
+
+    int pipefd[2];
+    int pid;
+    char recv[32];
+
+    pipe(pipefd);
+
+    switch(pid= fork()) {
+    case -1: perror("fork");
+	     exit(1);
+
+    case 0:    // in child process
+	close(pipefd[0]);       //close reading pipefd
+	FILE *out = fdopen(pipefd[1], "w"); // ope pipe as stream for writing
+	fprintf (out, "Howyoudoing(childpid:%d)\n", (int) getpid()); // write to stream
+	break;
+   default:               // in parent process
+	close(pipefd[1]);      	//close	writing	pipefd
+        FILE *in = fdopen(pipefd[0], "r"); // ope pipe as stream for reading
+        fscanf	(in, "%s", recv); // write to stream
+        printf (" Hello parent (pid:%d) received %s\n",(int)getpid(), recv);
+	break;
+
+     }
+}
+
+ *
+ */
 void runCommand(char input[]){
     //Array is always a min of 1 char to indicate end of array
     int count = 1;
@@ -81,12 +113,3 @@ int main() {
 
 
 }
-
-
-
-
-/**
- * {}
- * []
- * Sejr GitTest
- */
