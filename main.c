722bd87e244c42aa158b3f5c67fc9fd096abd96e
s195481 <s195481@student.dtu.dk>
2021-09-28 15:27:40 +0200

    Added runSingleCommand method and runPipeCommand method. Also did a bit of layout/comment/beautysalon thing

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+#include <sys/wait.h>
+
+void runSingleCommand(char **pString);
+
+void runPipeCommand(char **pString);
 
 char *getInput(int size) {
     char input[36];
@@ -35,6 +40,7 @@ char **splitString(char *array, char *delim) {
  */
 int main() {
     //Split for multiple args
+    int pipesFound = 0;
     char **holder = splitString(getInput(256), "|");
     printf("Listing: \n");
     int i = 0;
@@ -58,7 +64,69 @@ int main() {
         }
         i++;
     }
+    for (int j = 0; j < sizeof(holder); ++j) {
+        if (holder[j] == '|'){
+            pipesFound++;
+        }
+    }
+    if (pipesFound == 0){
+        runSingleCommand(holder);
+    }
+    else if (pipesFound > 0){
+        runPipeCommand(holder);
+    }
+}
+void runSingleCommand(char **command) {
+    int pid = fork();
+    // If forking failed
+    if (pid < 0){
+        fprintf(stderr, "Fork failed. \n");
+        exit(1);
+    }
+        // Child process creation succeeded
+    else if (pid == 0){
+        execvp(command[0],command);
+        printf("I am the child process with pid=%d\n", getpid());
+    }
+        // Child returns to parent
+    else if (pid > 1){
+        printf("I am the parent process with pid=%d\n", pid, getpid());
+    }
+}
 
+void runPipeCommand(char **pipeCommand) {
+    int pipefd[2];
+    int pid = fork();
+    char recv[32];
+    pipe(pipefd);
+    switch (pid) {
+        case -1:
+            perror("fork");
+            exit(1);
+            // In child process
+        case 0:
+            // Close reading pipefd
+            close(pipefd[0]);
+            // Open pipe as stream for writing
+            FILE *out = fdopen(pipefd[1], "w");
+            // Write to stream
+            fprintf(out, "This is a message to the stream from childpid:%d\n", (int) getpid());
+            exit(0);
+            break;
+            // In parent process
+        default:
+            // Close writing pipefd
+            close(pipefd[1]);
+            printf("Wait status: %d\n", wait(NULL));
+            printf("Read string: %s", recv);
+            // Open pipe as stream for reading
+            FILE *in = fdopen(pipefd[0], "r");
+            // Read from stream
+            fscanf(in, "%s", recv);
+            printf(" Hello parent (pid:%d) received %s\n", (int) getpid(), recv);
+
+    }
 
 }
+
 
