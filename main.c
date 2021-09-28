e44d4e73d4385373fe917e1877499400bc7e2bdf
SilasRindorf <s195474@student.dtu.dk>
2021-09-28 15:48:11 +0200

    Fix

--- main.c
+++ main.c
@@ -3,21 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
-#include <sys/wait.h>
-
-void runSingleCommand(char **pString);
-
-void runPipeCommand(char **pString);
 
 char *getInput(int size) {
-    char input[36];
+    char *input = malloc(size);
 
     //Get input
     char cwd[PATH_MAX];
     if (getcwd(cwd, sizeof(cwd)) != NULL) {
         printf("%s $ ", cwd);
-        fgets(input, 36, stdin);
+        fgets(input, size, stdin);
     }
+    return input;
 }
 
 
@@ -30,7 +26,7 @@ char **splitString(char *array, char *delim) {
         newarray[i++] = p;
         p = strtok(NULL, delim);
     }
-
+    newarray[0][i] = NULL;
     return newarray;
 }
 
@@ -40,7 +36,6 @@ char **splitString(char *array, char *delim) {
  */
 int main() {
     //Split for multiple args
-    int pipesFound = 0;
     char **holder = splitString(getInput(256), "|");
     printf("Listing: \n");
     int i = 0;
@@ -59,78 +54,14 @@ int main() {
          * s
          */
         while (sep[k] != NULL) {
-            printf("\t\t%s\n", sep[k]);
+            printf("\t\tsep with k=%d: ",k);
+            printf("%s\n", sep[k]);
+
             k++;
         }
-        runSingleCommand(sep);
         i++;
     }
-    /*for (int j = 0; j < sizeof(holder); ++j) {
-        if (holder[j] == '|'){
-            pipesFound++;
-        }
-    }
-    if (pipesFound == 0){
-        runSingleCommand(holder);
-    }
-    else if (pipesFound > 0){
-        runPipeCommand(holder);
-    }*/
-}
-void runSingleCommand(char **command) {
-    printf("Command is: %s\n", command[0]);
-    int rc = fork();
-    // If forking failed
-    if (rc < 0){
-        fprintf(stderr, "Fork failed. \n");
-        exit(1);
-    }
-    // Child process creation succeeded
-    else if (rc == 0){
-        execvp(&command[0][0],&command[0]);
-        // Does not execute and should not
-        printf("I am the child process with pid=%d\n", getpid());
-    }
-    // Child returns to parent
-    else if (rc > 1){
-        int vc = wait(NULL);
-        printf("I am the parent process with pid=%d\n", rc, getpid());
-    }
-}
 
-void runPipeCommand(char **pipeCommand) {
-    int pipefd[2];
-    int pid = fork();
-    char recv[32];
-    pipe(pipefd);
-    switch (pid) {
-        case -1:
-            perror("fork");
-            exit(1);
-            // In child process
-        case 0:
-            // Close reading pipefd
-            close(pipefd[0]);
-            // Open pipe as stream for writing
-            FILE *out = fdopen(pipefd[1], "w");
-            // Write to stream
-            fprintf(out, "This is a message to the stream from childpid:%d\n", (int) getpid());
-            exit(0);
-            break;
-            // In parent process
-        default:
-            // Close writing pipefd
-            close(pipefd[1]);
-            printf("Wait status: %d\n", wait(NULL));
-            printf("Read string: %s", recv);
-            // Open pipe as stream for reading
-            FILE *in = fdopen(pipefd[0], "r");
-            // Read from stream
-            fscanf(in, "%s", recv);
-            printf(" Hello parent (pid:%d) received %s\n", (int) getpid(), recv);
-
-    }
 
 }
-
 
