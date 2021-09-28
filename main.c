9c0c9131ec029f3eb2cf24dca1dcf950d2253867
SilasRindorf <s195474@student.dtu.dk>
2021-09-28 14:22:20 +0200

    Refactor big

--- main.c
+++ main.c
@@ -1,94 +1,32 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/wait.h>
 #include <string.h>
 #include <limits.h>
 
-void runCommand(char input[]) {
-    //Array is always a min of 1 char to indicate end of array
-    int count = 1;
-    int pipecheck = 0;
+char *getInput(int size) {
+    char input[36];
 
-    for (int i = 0; i < strlen(input); i++) {
-        if (input[i] == ' ') {
-            count++;
-        }
-            //Count how many arguments
-        else if (input[i] == '|') {
-            pipecheck = 1;
-            input[i] = NULL;
-        }
-            //Remove non argument char
-        else if (input[i] == '\n')
-            input[i] = NULL;
+    //Get input
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("%s $ ", cwd);
+        fgets(input, 36, stdin);
     }
-    char *nargs[1][count];
-    //Last pos is NULL to indicate end of array
-    //Since it's a pointer array
-    nargs[0][count] = NULL;
-    //Reuse counter
-    count = 0;
-    //Convert to pointer array
-    nargs[0][count] = strtok(input, " ");
-    count++;
-    //When we hit end of array
-    while (nargs[0][count] != NULL) {
-        nargs[0][count] = strtok(NULL, " ");
-        count++;
+}
+
+
+char **splitString(char *array, char *delim) {
+    int i = 0;
+    char *p = strtok(array, delim);
+    char **newarray = malloc(32);
+
+    while (p != NULL) {
+        newarray[i++] = p;
+        p = strtok(NULL, "|");
     }
-    count = 0;
-    //Prints and should print, status message
-    printf("Parent process booting up with pid=%d and command='%s'\n", getpid(), nargs[0][0]);
 
-    //Print argument temp print
-    while (nargs[0][count] != NULL) {
-        printf("\tArgument %d=%s\n", count, nargs[0][count]);
-        count++;
-    }
-    if (pipecheck == 0) {
-        //Create child process
-        int rc = fork();
-        //Fork-ing failed
-        if (rc < 0) {
-            fprintf(stderr, "fork failed\n");
-            exit(1);
-        } else if (rc == 0) { // Child process creation succeeded
-            //Execute command, first argument is the command to be executed, 2nd argument is arguments for the command being executed
-            execvp(nargs[0][0], nargs[0]);
-            //Does not execute and should not
-            printf("I am the child process with pid=%d!\n", getpid());
-
-        } else { //Child returns to parent
-            int wc = wait(NULL);
-            printf("I am the parent process of %d. I have pid=%d\n", rc, getpid());
-        }
-    } else {
-        int pipefd[2];
-        int pid = fork();
-        char recv[32];
-        pipe(pipefd);
-        switch (pid) {
-            case -1:
-                perror("fork");
-                exit(1);
-
-            case 0:    // in child process
-                close(pipefd[0]);       //close reading pipefd
-                FILE *out = fdopen(pipefd[1], "w"); // ope pipe as stream for writing
-                fprintf(out, "Howyoudoing(childpid:%d)\n", (int) getpid()); // write to stream
-                exit(0);
-                break;
-            default:               // in parent process
-                close(pipefd[1]);        //close	writing	pipefd
-                printf("Wait status: %d\n", wait(NULL));
-                printf("Read string: %s", recv);
-                FILE *in = fdopen(pipefd[0], "r"); // ope pipe as stream for reading
-                fscanf(in, "%s", recv); // write to stream
-                printf(" Hello parent (pid:%d) received %s\n", (int) getpid(), recv);
-
-        }
-    }
+    return newarray;
 }
 
 /***
@@ -96,21 +34,15 @@ void runCommand(char input[]) {
  * @return
  */
 int main() {
-    char input[36];
-    //Loop so on exit after excecution of command
-    do {
-        //Get input
-        char cwd[PATH_MAX];
-        if (getcwd(cwd, sizeof(cwd)) != NULL) {
-            printf("%s $ ", cwd);
-            fgets(input, 36, stdin);
-            //Run our method
-            runCommand(input);
-        } else { //Read error
-            perror("getcwd() error");
-            return 1;
-        }
-    } while (1);
+    //Split for multiple args
+    char **holder = splitString(getInput(32), "|");
+    //printf("Holder holds: %s", holder[0]);
+
+    int i = 0;
+    while (holder[i] != NULL) {
+        printf("Holder holds: %s", holder[i]);
+        i++;
+    }
 
 }
 
