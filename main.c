d2c837a38c6b8a4d3563f7a8130fa3ec53767655
MortencKruuse <Hiccup#1998>
2021-09-28 18:49:58 +0200

    Push for sejr

--- main.c
+++ main.c
@@ -35,7 +35,7 @@ char **splitString(char *array, char *delim) {
         newarray[i++] = p;
         p = strtok(NULL, delim);
     }
-    for (int j = 0; j < 256; ++j) {
+    for (int j = 0; j < 255; ++j) {
         if(newarray[0][j] == '\n'){
             newarray[0][j] = NULL;
             break;
@@ -107,7 +107,7 @@ void runPipeCommand(char **pipeCommand) {
             break;
     }
 }
-void runPipeCommandV2(char **pipeCommand) {
+void runPipeCommandV2(char **pipeCommand, char **pipeCommand2) {
     int pipefd[2];
     pid_t process1, process2;
     if (pipe(pipefd) < 0){
@@ -115,29 +115,32 @@ void runPipeCommandV2(char **pipeCommand) {
         return;
     }
     process1 = fork();
-    if (process1 > 0){
-        printf("\nForking failed.")
+    if (process1 < 0 ){
+        printf("\nForking failed.");
     }
-    if (process1 == 0) {
+    if (process1 == 0 ) {
         close(pipefd[0]);
         // Using "STDOUT_FILENO" as it is an integer file descriptor (actually, the integer 1)
-        dup2(pipefd[1],STDOUT_FILENO);
+        FILE *out = fdopen(pipefd[1], "w"); // ope pipe as stream for writing
+        // dup2(pipefd[1],STDOUT_FILENO);
         close(pipefd[1]);
+
+        printf("\nExecution of first command did stuffs.");
         if (execvp(pipeCommand[0], pipeCommand) < 0){
             printf("\nExecution of first command failed.");
             exit(0);
         }
     } else {
         process2 = fork();
-        if (process2 > 0){
+        if (process2 < 0 ){
             printf("\nForking failed.");
         }
         if (process2 == 0){
             close(pipefd[1]);
             // Using "STDOUT_FILENO" as it is an integer file descriptor (actually, the integer 1)
-            dup2(pipefd[0],STDOUT_FILENO);
+            dup2(pipefd[0],STDIN_FILENO);
             close(pipefd[0]);
-            if (execvp(pipeCommand[0], pipeCommand) < 0){
+            if (execvp(pipeCommand2[0], pipeCommand2) < 0){
                 printf("\nExecution of second command failed.");
                 exit(0);
             }
@@ -145,6 +148,71 @@ void runPipeCommandV2(char **pipeCommand) {
             wait(NULL);
             wait(NULL);
         }
+    }
+}
+void runPipeCommandV3(char **pipeCommand){
+    int pipefd[2];
+    int pid;
+    char recv[32];
+
+    pipe(pipefd);
+
+    switch(pid= fork()) {
+        case -1: perror("fork");
+            exit(1);
+
+        case 0:    // in child process
+            close(pipefd[0]);       //close reading pipefd
+            FILE *out = fdopen(pipefd[1], "w"); // ope pipe as stream for writing
+            fprintf (out, "Howyoudoing(childpid:%d)\n", (int) getpid()); // write to stream
+            break;
+        default:               // in parent process
+            close(pipefd[1]);      	//close	writing	pipefd
+            FILE *in = fdopen(pipefd[0], "r"); // ope pipe as stream for reading
+            fscanf	(in, "%s", recv); // write to stream
+            printf (" Hello parent (pid:%d) received %s\n",(int)getpid(), recv);
+            break;
+
+    }
+}
+
+void runPipeCommandV4(char **pipecommand){
+    int pipefd[2];
+    pid_t cpid;
+    char buf;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <string>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    cpid = fork();
+    if (cpid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (cpid == 0) {    /* Child reads from pipe */
+        close(pipefd[1]);          /* Close unused write end */
+
+        while (read(pipefd[0], &buf, 1) > 0)
+            write(STDOUT_FILENO, &buf, 1);
+
+        write(STDOUT_FILENO, "\n", 1);
+        close(pipefd[0]);
+        _exit(EXIT_SUCCESS);
+
+    } else {            /* Parent writes argv[1] to pipe */
+        close(pipefd[0]);          /* Close unused read end */
+        write(pipefd[1], argv[1], strlen(argv[1]));
+        close(pipefd[1]);          /* Reader will see EOF */
+        wait(NULL);                /* Wait for child */
+        exit(EXIT_SUCCESS);
     }
 }
 
@@ -170,7 +238,12 @@ int main() {
          * ws
          */
       //  while (holder[i] != NULL) {
+            char ** sep2;
             char ** sep = splitString(holder[i]," ");
+            if (holder[i+1] != NULL ){
+                sep2 = splitString(holder[i+1]," ");
+            }
+
             int k = 0;
             printf("\tSep: \n");
             /**
@@ -183,11 +256,17 @@ int main() {
                 printf("%s\n", sep[k]);
                 k++;
             }
+            int v = 0;
+            while (sep2[v] != NULL) {
+                printf("\t\tsep with v=%d: ",v);
+                printf("%s\n", sep2[v]);
+                v++;
+        }
             if(pipesFound == 0){
                 runSingleCommand(sep);
             }
             else{
-                runPipeCommand(sep);
+                runPipeCommandV2(sep, sep2);
             }
             i++;
   //  }
