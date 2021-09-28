2dbff63543181e24d3e7167aea51653f6a5b5be2
MortencKruuse <Hiccup#1998>
2021-09-28 19:09:29 +0200

    Normal ls works

--- main.c
+++ main.c
@@ -165,45 +165,6 @@ void runPipeCommandV3(char **pipeCommand){
     }
 }
 
-void runPipeCommandV4(char **pipecommand){
-    int pipefd[2];
-    pid_t cpid;
-    char buf;
-
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <string>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    if (pipe(pipefd) == -1) {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }
-
-    cpid = fork();
-    if (cpid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-
-    if (cpid == 0) {    /* Child reads from pipe */
-        close(pipefd[1]);          /* Close unused write end */
-
-        while (read(pipefd[0], &buf, 1) > 0)
-            write(STDOUT_FILENO, &buf, 1);
-
-        write(STDOUT_FILENO, "\n", 1);
-        close(pipefd[0]);
-        _exit(EXIT_SUCCESS);
-
-    } else {            /* Parent writes argv[1] to pipe */
-        close(pipefd[0]);          /* Close unused read end */
-        write(pipefd[1], argv[1], strlen(argv[1]));
-        close(pipefd[1]);          /* Reader will see EOF */
-        wait(NULL);                /* Wait for child */
-        exit(EXIT_SUCCESS);
-    }
-}
 
 /***
  *
@@ -246,10 +207,13 @@ int main() {
                 k++;
             }
             int v = 0;
-            while (sep2[v] != NULL) {
-                printf("\t\tsep with v=%d: ",v);
-                printf("%s\n", sep2[v]);
-                v++;
+            if(sep2 != NULL){
+                while (sep2[v] != NULL) {
+                    printf("\t\tsep with v=%d: ",v);
+                    printf("%s\n", sep2[v]);
+                    v++;
+            }
+
         }
             if(pipesFound == 0){
                 runSingleCommand(sep);
