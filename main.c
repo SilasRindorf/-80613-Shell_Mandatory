84f67b203b8d32dc3a662b13b9d81c1146ebe631
SilasRindorf <s195474@student.dtu.dk>
2021-09-29 10:21:05 +0200

    Made pipe thing

--- main.c
+++ main.c
@@ -98,33 +98,4 @@ void runSingleCommand(char **command) {
     }
 }
 
-char * runPipeCommand(char **pipeCommand) {
-    int pipefd[2];
-    int pid;
-    char recv[32];
 
-    pipe(pipefd);
-    switch (pid=fork()) {
-        case -1:
-            perror("fork");
-            exit(1);
-            // In child process
-        case 0:
-            // Close reading pipefd
-            close(pipefd[0]);
-            // Open pipe as stream for writing
-            FILE *out = fdopen(pipefd[1], "w");
-            // Write to stream
-            fprintf(out, "howyoudoing(childpid:%d)\n", (int) getpid());
-            break;
-            // In parent process
-        default:
-            // Close writing pipefd
-            close(pipefd[1]);
-            // Open pipe as stream for reading
-            FILE *in = fdopen(pipefd[0], "r");
-            // Read from stream
-            fscanf(in, "%s", recv);
-            printf(" Hello parent (pid:%d) received %s\n", (int) getpid(), recv);
-    }
-}
