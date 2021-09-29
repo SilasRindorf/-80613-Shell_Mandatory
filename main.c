998f10edbbcbf1b6fbf20ae5127140b3ed8c31ac
SilasRindorf <s195474@student.dtu.dk>
2021-09-29 17:02:20 +0200

    Made pipe thing

--- main.c
+++ main.c
@@ -7,8 +7,10 @@
 #include "given-files/pipe.h"
 
 void runSingleCommand(char **command);
-char * runPipeCommand(char **pipeCommand);
-char * piperering(char ** args);
+
+char *runPipeCommand(char **pipeCommand);
+
+char *piperering(char **args);
 
 char *getInput(int size) {
     char *input = malloc(size);
@@ -27,14 +29,21 @@ char **splitString(char *array, char *delim) {
     int i = 0;
     char *p = strtok(array, "\n");
     p = strtok(p, delim);
-    char **newarray = malloc(256);
+    char **newarray = malloc(sizeof(array));
 
-    while (p != NULL) {
+    while (p) {
         newarray[i++] = p;
+        printf("Help=%li\n",strlen(p));
+        newarray[i] = realloc(newarray[i],sizeof(char*) * (strlen(p)-1));
+        //newarray[i][strlen(p)-1] = NULL;
         p = strtok(NULL, delim);
     }
-
-    newarray[0][255] = NULL;
+    //newarray = realloc(newarray, sizeof(char**) * (i+1));
+    /*int k = 0;
+    while (newarray[k] != NULL){
+        printf("Help=%li\n",(&newarray[k])[1] - newarray[k]);
+        k++;
+    }*/
     return newarray;
 }
 
@@ -43,57 +52,57 @@ char **splitString(char *array, char *delim) {
  * @return
  */
 int main() {
-
-    //Split for multiple args
-    int pipesFound = 0;
-    char **holder = splitString(getInput(256), "|");
-    int i = 0;
-    /**
-     * This while loop splits from '|', so
-     * ls "|"
-     * ws
-     */
-
-    while (holder[i] != NULL) {
-        char ** sep = splitString(holder[i]," ");
-        int k = 0;
-        //printf("\tSep: \n");*/
+    while (1) {
+        //Split for multiple args
+        char **holder = splitString(getInput(256), "|");
+        int i = 0;
         /**
-         * This while loop splits from ' ', so
-         * w " "
-         * s
+         * This while loop splits from '|', so
+         * ls "|"
+         * ws
          */
-        while (sep[k] != NULL) {
-            //printf("\t\tsep with k=%d: ",k);
-            //printf("%s\n", sep[k]);
-            k++;
-        }
-        //char *recv = runPipeCommand(sep);
-        //runSingleCommand(sep);
-        i++;
-        //piperering();
-        piperering(sep);
-        //char *recv = getInput(256);
-        printf("in main recv=%s",recv);
-    }
 
+        while (holder[i] != NULL) {
+            char **sep = splitString(holder[i], " ");
+            int k = 0;
+            printf("\tSerperating: %s\n",holder[i]);
+            /**
+             * This while loop splits from ' ', so
+             * w " "
+             * s
+             */
+            while (sep[k] != NULL) {
+                printf("\t\tsep with k=%d: ",k);
+                printf("%s\n", sep[k]);
+                k++;
+            }
+            //char *recv = runPipeCommand(sep);
+            runSingleCommand(sep);
+            i++;
+            //piperering();
+            //piperering(sep);
+            //char *recv = getInput(256);
+            //printf("in main recv=%s",recv);
+        }
+    }
 }
+
 void runSingleCommand(char **command) {
     printf("Command is: %s\n", command[0]);
     int rc = fork();
     // If forking failed
-    if (rc < 0){
+    if (rc < 0) {
         fprintf(stderr, "Fork failed. \n");
         exit(1);
     }
-    // Child process creation succeeded
-    else if (rc == 0){
-        execvp(&command[0][0],&command[0]);
+        // Child process creation succeeded
+    else if (rc == 0) {
+        execvp(&command[0][0], &command[0]);
         // Does not execute and should not
         printf("I am the child process with pid=%d\n", getpid());
     }
-    // Child returns to parent
-    else if (rc > 1){
+        // Child returns to parent
+    else if (rc > 1) {
         int vc = wait(NULL);
         printf("I am the parent process with pid=%d\n", rc, getpid());
     }
