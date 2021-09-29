d45d5f6810a48f56ca11a72fd0f26053d2fafeab
SilasRindorf <s195474@student.dtu.dk>
2021-09-29 09:45:52 +0200

    Made pipe thing

---
+++ given-files/pipe.h
@@ -0,0 +1,8 @@
+//
+// Created by server on 9/29/21.
+//
+
+#ifndef SHELL_MANDATORY_PIPE_H
+#define SHELL_MANDATORY_PIPE_H
+
+#endif //SHELL_MANDATORY_PIPE_H
--- main.c
+++ main.c
@@ -4,9 +4,11 @@
 #include <string.h>
 #include <limits.h>
 #include <sys/wait.h>
+#include "given-files/pipe.h"
 
 void runSingleCommand(char **command);
-void runPipeCommand(char **pipeCommand);
+char * runPipeCommand(char **pipeCommand);
+char * piperering(char ** args);
 
 char *getInput(int size) {
     char *input = malloc(size);
@@ -23,14 +25,16 @@ char *getInput(int size) {
 
 char **splitString(char *array, char *delim) {
     int i = 0;
-    char *p = strtok(array, delim);
+    char *p = strtok(array, "\n");
+    p = strtok(p, delim);
     char **newarray = malloc(256);
 
     while (p != NULL) {
         newarray[i++] = p;
         p = strtok(NULL, delim);
     }
-    newarray[0][i+1] = NULL;
+
+    newarray[0][255] = NULL;
     return newarray;
 }
 
@@ -39,44 +43,39 @@ char **splitString(char *array, char *delim) {
  * @return
  */
 int main() {
+
     //Split for multiple args
     int pipesFound = 0;
     char **holder = splitString(getInput(256), "|");
-    printf("Listing: \n");
     int i = 0;
     /**
      * This while loop splits from '|', so
      * ls "|"
      * ws
      */
+
     while (holder[i] != NULL) {
         char ** sep = splitString(holder[i]," ");
         int k = 0;
-        printf("\tSep: \n");
+        //printf("\tSep: \n");*/
         /**
          * This while loop splits from ' ', so
          * w " "
          * s
          */
         while (sep[k] != NULL) {
-            printf("\t\tsep with k=%d: ",k);
-            printf("%s\n", sep[k]);
+            //printf("\t\tsep with k=%d: ",k);
+            //printf("%s\n", sep[k]);
             k++;
         }
-        runSingleCommand(sep);
+        //char *recv = runPipeCommand(sep);
+        //runSingleCommand(sep);
         i++;
+        //piperering();
+        char *recv = piperering(sep);
+        printf("in main recv=%s",recv);
     }
-    /*for (int j = 0; j < sizeof(holder); ++j) {
-        if (holder[j] == '|'){
-            pipesFound++;
-        }
-    }
-    if (pipesFound == 0){
-        runSingleCommand(holder);
-    }
-    else if (pipesFound > 0){
-        runPipeCommand(holder);
-    }*/
+
 }
 void runSingleCommand(char **command) {
     printf("Command is: %s\n", command[0]);
@@ -99,12 +98,13 @@ void runSingleCommand(char **command) {
     }
 }
 
-void runPipeCommand(char **pipeCommand) {
+char * runPipeCommand(char **pipeCommand) {
     int pipefd[2];
-    int pid = fork();
+    int pid;
     char recv[32];
+
     pipe(pipefd);
-    switch (pid) {
+    switch (pid=fork()) {
         case -1:
             perror("fork");
             exit(1);
@@ -115,20 +115,16 @@ void runPipeCommand(char **pipeCommand) {
             // Open pipe as stream for writing
             FILE *out = fdopen(pipefd[1], "w");
             // Write to stream
-            fprintf(out, "This is a message to the stream from childpid:%d\n", (int) getpid());
-            exit(0);
+            fprintf(out, "howyoudoing(childpid:%d)\n", (int) getpid());
             break;
             // In parent process
         default:
             // Close writing pipefd
             close(pipefd[1]);
-            printf("Wait status: %d\n", wait(NULL));
-            printf("Read string: %s", recv);
             // Open pipe as stream for reading
             FILE *in = fdopen(pipefd[0], "r");
             // Read from stream
             fscanf(in, "%s", recv);
             printf(" Hello parent (pid:%d) received %s\n", (int) getpid(), recv);
-
     }
 }
